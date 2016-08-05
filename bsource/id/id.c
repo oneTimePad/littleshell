5ea00b9ffc1dfb93328d147d4936f5290ff88858
Dylan Herman <dah242@scarletmail.rutgers.edu>
2016-08-05 18:15:15 -0400

    id compiled

---
+++ bsource/id/id.c
@@ -0,0 +1,100 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <pwd.h>
+#include <grp.h>
+#include <errno.h>
+#include "../../errors.h"
+#include "id.h"
+
+
+
+/**
+* converts gid into corresponding name
+* gid: id to converts
+* name: buffer with enough space to hold grp name and null-term
+* buf_size: size of buf
+* returns: int status, sets errno
+**/
+_BOOL getnamefromgid(gid_t gid, char *name, size_t buf_size){
+
+  struct group * grp;
+  errno = 0;
+  if((grp = getgrgid(gid))==NULL)
+    return FALSE;
+  errno = 0;
+  if(NOMEMBUF(buf_size,grp->gr_name)){
+    errno = ENOMEM;
+    return FALSE;
+  }
+
+  strncpy(name,grp->gr_name,buf_size);
+  return TRUE;
+
+}
+
+/**
+* converts uid into corresponding name
+* uid: id to converts
+* name: buffer with enough space to hold usr name and null-term
+* buf_size: size of buf
+* returns: int status, sets errno
+**/
+_BOOL getnamefromuid(uid_t uid, char *name, size_t buf_size){
+  struct passwd* pw;
+  errno = 0;
+  if((pw = getpwuid(uid)) == NULL)
+    return FALSE;
+  errno = 0;
+  if(NOMEMBUF(buf_size,pw->pw_name)){
+    errno = ENOMEM;
+    return FALSE;
+  }
+
+  strncpy(name,pw->pw_name,buf_size);
+  return TRUE;
+}
+
+/**
+* retrieve user id information
+* returns: static user struct
+**/
+USER * getuserinfo(void){
+  static USER useri;
+  memset(&useri,0,sizeof(USER));
+  #ifdef GNU_SOURCE
+  errno = 0;
+  if(getresuid(&useri.rid,&useri.eid,&useri.suid) == -1)
+      return NULL; //errno is set, return null
+
+  if(getresgid(&useri.rgid,&useri.egid,&useri.sgid) == -1)
+      return NULL;
+  #else
+
+  useri.rid = getuid();
+  useri.eid = geteuid();
+  useri.suid = NOT_APPL;
+
+  useri.rgid = getgid();
+  useri.egid = getegid();
+  useri.sgid = NOT_APPL;
+
+  #endif
+  errno = 0;
+  if(getgroups(NGROUPS_MAX+1,useri.grouplist) == -1)
+    return NULL;
+
+  return &useri;
+}
+
+
+
+
+int
+main(int argc, char* argv[]){
+
+
+
+
+  exit(EXIT_SUCCESS);
+}
---
+++ bsource/id/id.h
@@ -0,0 +1,32 @@
+#ifndef ID_H
+#define ID_H
+#include <sys/types.h>
+#include <unistd.h>
+#include <limits.h>
+#include <string.h>
+#include "../../bool.h"
+#define NOT_APPL (long)-1
+#define NOMEMBUF(buf, str)  (strlen(str) + 1 > buf) ? TRUE: FALSE
+
+typedef struct PROCESS_USER{
+
+  uid_t rid; //real uid
+  uid_t eid; //eff uid
+  uid_t suid; // saved-set uid
+
+
+  gid_t rgid;  //real gid
+  gid_t egid; //eff gid
+  gid_t sgid;  // saved-set gid
+
+  gid_t grouplist[NGROUPS_MAX+1]; //supp gids
+
+
+
+}USER;
+
+_BOOL getnamefromgid(gid_t,char *, size_t);
+_BOOL getnamefromuid(uid_t, char *, size_t);
+USER * getuserinfo(void);
+
+#endif
