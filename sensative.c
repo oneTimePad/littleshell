2e73b08e61ff89d3d066605df6c91b0a06ecbe72
Dylan Herman <dah242@scarletmail.rutgers.edu>
2016-08-28 23:07:22 -0400

    implemented prehandler functions

--- sensative.c
+++ sensative.c
@@ -6,12 +6,12 @@ static prehandler pre_handlers[] ={
   pre_pipe_handler,
   pre_redirstdin_handler,
   pre_redirstdout_handler,
-  pre_and_handler,
   pre_background_handler,
+  pre_and_handler,
   NULL
 };
 
-static posthandler post_handlers[]{
+static posthandler post_handlers[] = {
   post_pipe_handler,
   post_redirstdin_handler,
   post_redirstdout_handler,
@@ -20,7 +20,7 @@ static posthandler post_handlers[]{
 
 
 
-_BOOL pre_pipe_handler(EMBRYO *embryos,EMBRYO_INFO *info,char which){
+_BOOL pre_pipe_handler(EMBRYO *procs,EMBRYO_INFO *info,char which){
   int pipes[2];
   //invalid if : there is no current process, a pipe is already present, or the current process is backgrounded( i.e proc1 & | proc2 is invalid)
   if(info->cur_proc == -1 || info->continuing|| procs[info->cur_proc].p_stdout!=-1 || info->last_sequence != '\0' || *procs[info->cur_proc].background){
@@ -36,20 +36,51 @@ _BOOL pre_pipe_handler(EMBRYO *embryos,EMBRYO_INFO *info,char which){
   info->last_sequence = PIPE;
 }
 
-_BOOL pre_redirstdin_handler(EMBRYO *embryos, EMBRYO_INFO *info,char which){
+_BOOL pre_redirstdin_handler(EMBRYO *procs, EMBRYO_INFO *info,char which){
+  if(info->cur_proc == -1 || info->last_sequence!='\0' || info->continuing || procs[info->cur_proc].p_stdin!=-1){
+    errno = EINVAL;
+    return FALSE;
 
+  }
+  procs[info->cur_proc].num_components_job_name++;
+  info->last_sequence = which;
+  return TRUE;
 }
 _BOOL pre_redirstdout_handler(EMBRYO *embryos, EMBRYO_INFO *info,char which){
-
-}
-
-_BOOL pre_and_handler(EMBRYO *embryos, EMBRYO_INFO *info, char which){
-
+  if(info->cur_proc == -1 || info->last_sequence!='\0' || info->continuing || procs[info->cur_proc].p_stdout!=-1){
+    errno = EINVAL;
+    return FALSE;
+  }
+  procs[info->cur_proc].num_components_job_name++;
+  info->last_sequence = which;
+  return TRUE;
 }
 
 _BOOL pre_background_handler(EMBRYO *embryos, EMBRYO_INFO *info, char which){
-
+  if(info->cur_proc == -1|| info->continuing || info->last_sequence != '\0'){
+    errno = EINVAL;
+    return FALSE;
+  }
+  //set all processes in pipe to background
+  *procs[info->cur_proc].background = TRUE;
+  procs[info->cur_proc].num_components_job_name++;
+  info->fork_seq++;
+  info->last_sequence = which;
+  return TRUE;
 }
+
+_BOOL pre_and_handler(EMBRYO *embryos, EMBRYO_INFO *info, char which){
+  if(info->cur_proc == -1 || info->continuing ){
+    errno = EINVAL;
+    return FALSE;
+  }
+  info->fork_seq++;
+  procs[info->cur_proc].num_components_job_name++;
+  info->last_sequence = which;
+  return TRUE;
+}
+
+
 
 
 _BOOL post_pipe_handler(EMBRYO *embryos,EMBRYO_INFO *info, EMBRYO *new_proc){
